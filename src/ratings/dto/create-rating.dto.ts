b302fce3968ecff0113e56c93e6404e8747e1d7b
gbubemi22 <slimnfine22@gmail.com>
2024-10-08 17:51:49 +0100

    rating

--- src/ratings/dto/create-rating.dto.ts
+++ src/ratings/dto/create-rating.dto.ts
@@ -1,1 +1,28 @@
-export class CreateRatingDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateRatingDto {
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  productId: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  @Max(5)
+  rating: number;
+
+  @IsOptional()
+  @IsString()
+  comment?: string;
+}
